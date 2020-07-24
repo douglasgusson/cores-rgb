21a9bda86237eb8b19afc03760664cf279dc899e
Douglas Gusson <douglasgusson@gmail.com>
2020-07-24 20:51:41 -0300

    Implementação conversão cruzada

--- js/app.js
+++ js/app.js
@@ -3,13 +3,10 @@ const app = new Vue({
     data: {
         red: 0,
         green: 0,
-        blue: 0
+        blue: 0,
+        cor: "#000000"
     },
     computed: {
-        cor: function () {
-            return `#${this.redHex}${this.greenHex}${this.blueHex}`
-        },
-
         redHex: function () {
             return this.decimalParaHexa(this.red);
         },
@@ -22,9 +19,48 @@ const app = new Vue({
             return this.decimalParaHexa(this.blue);
         },
     },
+    watch: {
+        cor: {
+            handler: function (newVal) {
+                this.formatarCorHex(newVal);
+            },
+            deep: true
+        },
+        red: {
+            handler: function () {
+                this.atualizarCor();
+            },
+            deep: true
+        },
+        green: {
+            handler: function () {
+                this.atualizarCor();
+            },
+            deep: true
+        },
+        blue: {
+            handler: function () {
+                this.atualizarCor();
+            },
+            deep: true
+        },
+    },
     methods: {
         decimalParaHexa(valor) {
             return ("0" + (Number(valor).toString(16))).slice(-2).toUpperCase();
+        },
+
+        atualizarCor() {
+            this.cor = "#" + this.redHex + this.greenHex + this.blueHex;
+        },
+
+        formatarCorHex(val) {
+            val = val.replaceAll("#", "");
+            if (val.length == 6) {
+                this.red = parseInt("0x" + val.substr(0, 2));
+                this.green = parseInt("0x" + val.substr(2, 2));
+                this.blue = parseInt("0x" + val.substr(4, 2));
+            }
         },
     }
 })
